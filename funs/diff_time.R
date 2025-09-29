d878716f3a510dd74b0113f3ab56eb09aa7b5436
Sebastian Sauer <ssauer@posteo.de>
2025-09-29 09:52:35 +0200

    initial commit

--- funs/diff_time.R
+++
@@ -1,9 +0,0 @@
-diff_time <- function(data, group = id_visit, time = time) {
-  data |> 
-    filter(type ==  "timestamp") |> 
-    group_by({{ group }}) |> 
-    mutate(type = as.character(type)) |> 
-    mutate(time = parse_date_time(value, "ymd HMS"),
-            time_diff = max(time) - min(time)) 
-}
-
